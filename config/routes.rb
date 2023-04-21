83b5c5a6ac33bf658660340a1c69926696493e07
Hazel Chui <hazelchui@uchicago.edu>
2023-04-21 05:56:31 +0000

    halfway

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get("/directors/eldest", { :controller => "directors", :action => "wisest" })
+
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+
 end
